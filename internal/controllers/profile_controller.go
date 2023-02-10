dfd48537569c58c47e94a8bfaf75c2dcf701aaf1
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2023-02-10 02:05:00 +0200

    refactoring: merge controllers

--- internal/controllers/profile_controller.go
+++ internal/controllers/profile_controller.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/u8views/go-u8views/internal/services"
 	"github.com/u8views/go-u8views/internal/storage/dbs"
@@ -17,16 +18,71 @@ type ProfileURI struct {
 	Username string `uri:"username" binding:"required"`
 }
 
-type ProfileController struct {
+type WebController struct {
 	userService         *services.UserService
 	profileStatsService *services.ProfileStatsService
 }
 
-func NewProfileController(userService *services.UserService, profileStatsService *services.ProfileStatsService) *ProfileController {
-	return &ProfileController{userService: userService, profileStatsService: profileStatsService}
+func NewWebController(userService *services.UserService, profileStatsService *services.ProfileStatsService) *WebController {
+	return &WebController{userService: userService, profileStatsService: profileStatsService}
 }
 
-func (c *ProfileController) GitHubProfile(ctx *gin.Context) {
+func (c *WebController) Index(ctx *gin.Context) {
+	const limit = 32
+
+	users, err := c.userService.Users(ctx, limit)
+	if err != nil {
+		log.Printf("Cannot fetch users %s\n", err)
+
+		ctx.Data(http.StatusOK, "text/html; charset=utf-8", []byte(templates.Index(nil)))
+
+		return
+	}
+
+	if len(users) == 0 {
+		ctx.Data(http.StatusOK, "text/html; charset=utf-8", []byte(templates.Index(nil)))
+
+		return
+	}
+
+	userIDs := make([]int64, len(users))
+	for i, user := range users {
+		userIDs[i] = user.ID
+	}
+
+	now := time.Now().UTC().Truncate(time.Hour)
+	userViewsStatsMap, err := c.profileStatsService.UserDayWeekMonthViewsStatsMap(ctx, userIDs, now)
+	if err != nil {
+		log.Printf("Cannot fetch stats %s\n", err)
+
+		// NOP
+	}
+
+	profiles := make([]templates.FullProfileView, len(users))
+	for i, user := range users {
+		stats := userViewsStatsMap[user.ID]
+
+		profiles[i] = templates.FullProfileView{
+			ProfileView: templates.ProfileView{
+				ID:                   user.ID,
+				SocialProviderUserID: user.SocialProviderUserID,
+				Username:             user.Username,
+				Name:                 user.Name,
+			},
+			ProfileViewsStats: templates.ProfileViewsStats{
+				DayCount:   stats.DayCount,
+				WeekCount:  stats.WeekCount,
+				MonthCount: stats.MonthCount,
+				TotalCount: user.TotalCount,
+			},
+			CreatedAt: user.CreatedAt,
+		}
+	}
+
+	ctx.Data(http.StatusOK, "text/html; charset=utf-8", []byte(templates.Index(profiles)))
+}
+
+func (c *WebController) GitHubProfile(ctx *gin.Context) {
 	var uri ProfileURI
 
 	err := ctx.ShouldBindUri(&uri)
@@ -84,4 +140,8 @@ func (c *ProfileController) GitHubProfile(ctx *gin.Context) {
 		currentUserView,
 		stats,
 	)))
+}
+
+func (c *WebController) Stats(ctx *gin.Context) {
+
 }
