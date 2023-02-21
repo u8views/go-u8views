265cebc595f051dc1aabcbbf9aa47ed33f0d8a0d
Yaroslav Podorvanov <63663261+YaroslavPodorvanov@users.noreply.github.com>
2023-02-21 02:06:26 +0200

    instruction repository update status (#20)

--- client/src/instruction.ts
+++ client/src/instruction.ts
@@ -1,4 +1,9 @@
-export function connectInstructionToggle() {
+export function initInstruction() {
+    initToggle();
+    updateInstructionRepositoryExistsStatus();
+}
+
+function initToggle() {
     const $button = document.querySelector(".js-instruction-toggle-button");
     const $state = document.querySelector(".js-instruction-visibility-state");
     const $instruction = document.querySelector(".js-instruction");
@@ -7,4 +12,22 @@ export function connectInstructionToggle() {
         $instruction.classList.toggle("hide");
         $state.classList.toggle("active");
     });
+}
+
+function updateInstructionRepositoryExistsStatus() {
+    const $step = document.querySelector(".js-instruction-repository-exists");
+    if ($step.classList.contains("active")) {
+        return;
+    }
+
+    const username = document.body.getAttribute("data-session-profile-username");
+    if (username === "") {
+        return;
+    }
+
+    fetch(`https://api.github.com/repos/${username}/${username}`)
+        .then(function (response) {
+            $step.classList.add("active")
+        })
+        .catch(console.error);
 }
