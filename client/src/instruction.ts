ea746d83cd73ca34d90b3ce070efc2aa5f22122d
VictorPoprozhuk <victor.poprozhuk@gmail.com>
2023-03-02 22:35:01 +0200

    updated static html files and instruction.ts in client

--- client/src/instruction.ts
+++ client/src/instruction.ts
@@ -5,12 +5,10 @@ export function initInstruction() {
 
 function initToggle() {
     const $button = document.querySelector(".js-instruction-toggle-button");
-    const $state = document.querySelector(".js-instruction-visibility-state");
     const $instruction = document.querySelector(".js-instruction");
 
     $button.addEventListener("click", () => {
-        $instruction.classList.toggle("hide");
-        $state.classList.toggle("active");
+        $instruction.classList.toggle("active");
     });
 }
 
