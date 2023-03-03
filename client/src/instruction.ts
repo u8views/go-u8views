ef96324666dd4e450a10963b8a4728a0edbb4b8a
VictorPoprozhuk <victor.poprozhuk@gmail.com>
2023-03-03 11:48:15 +0200

    fix css

--- client/src/instruction.ts
+++ client/src/instruction.ts
@@ -5,10 +5,12 @@ export function initInstruction() {
 
 function initToggle() {
     const $button = document.querySelector(".js-instruction-toggle-button");
+    const $state = document.querySelector(".js-instruction-visibility-state");
     const $instruction = document.querySelector(".js-instruction");
 
     $button.addEventListener("click", () => {
-        $instruction.classList.toggle("active");
+        $instruction.classList.toggle("hide");
+        $state.classList.toggle("active");
     });
 }
 
