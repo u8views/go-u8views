75f815dba6c42d8588adca9369fd9dc08e40aaa8
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2025-04-03 01:46:13 +0300

    Articles & Stats

--- console/postgres-fixtures.sql
+++ console/postgres-fixtures.sql
@@ -6,22 +6,22 @@ TRUNCATE users CASCADE;
 
 INSERT INTO users (id)
 SELECT generated_id
-FROM generate_series(1, 10 * 1000) AS generated_id;
+FROM GENERATE_SERIES(1, 10 * 1000) AS generated_id;
 
 INSERT INTO profile_total_views (user_id, count)
 SELECT generated_user_id, 876100
-FROM generate_series(1, 10 * 1000) AS generated_user_id;
+FROM GENERATE_SERIES(1, 10 * 1000) AS generated_user_id;
 
 -- 87,610,000 rows affected in 19 m 8 s 769 ms
 INSERT INTO profile_hourly_views_stats (time, user_id, count)
 SELECT generated_time, generated_user_id, generated_user_id % 100 + 1
-FROM generate_series(
-             (DATE_TRUNC('hour', NOW()) - INTERVAL '1 MONTH')::TIMESTAMP,
-             (DATE_TRUNC('hour', NOW()))::TIMESTAMP,
-             '1 hour'::INTERVAL
+FROM GENERATE_SERIES(
+             (DATE_TRUNC('HOUR', NOW()) - INTERVAL '1 MONTH')::TIMESTAMP,
+             (DATE_TRUNC('HOUR', NOW()))::TIMESTAMP,
+             '1 HOUR'::INTERVAL
          ) AS generated_time
          INNER JOIN
-     generate_series(
+     GENERATE_SERIES(
              1,
              10 * 1000,
              1
