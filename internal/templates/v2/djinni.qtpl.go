8d99a9899003c64a886d5c92a61b8b9ccebee25f
Yaroslav Podorvanov <yaroslav.podorvanov@gmail.com>
2025-12-27 22:46:41 +0300

    Testing

--- internal/templates/v2/djinni.qtpl.go
+++ internal/templates/v2/djinni.qtpl.go
@@ -22,7 +22,17 @@ func streamdjinni(qw422016 *qt422016.Writer) {
 		alt="Anonymous job search for developers – Djinni"
 	/>
 	<figcaption class="hero__figcaption">
-		Anonymous job search for developers and everyone working in tech.
+		`)
+	if djinniSignUpOrJobs() {
+		qw422016.N().S(`
+			Looking for a job? Try anonymous job search: you create a profile, recruiters reach out to you, and you share your contacts only with those you like.
+		`)
+	} else {
+		qw422016.N().S(`
+			Anonymous job search for developers and everyone working in tech.
+		`)
+	}
+	qw422016.N().S(`
 	</figcaption>
 </figure>
 <button class="hero__donate-button">
